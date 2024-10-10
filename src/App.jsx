b1055791ba527eefc6ffd76196ec6d1773295740
jtroubleK <149900298+jbellido89@users.noreply.github.com>
2024-10-10 19:33:06 +0200

    Refactor App component to utilize Movies component for better structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import responseMovies from './mocks/with-results.json';
 import noResults from './mocks/no-results.json';
+import { Movies } from './components/Movies';
 
 function App() {
   const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
   return (
     <div className='page'>
       <header>
@@ -16,19 +16,7 @@ function App() {
       </header>
 
       <main>
-        {hasMovies ? (
-          <ul>
-            {movies.map((movie) => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No se encontraron resultados</p>
-        )}
+        <Movies movies={movies} />
       </main>
     </div>
   );
