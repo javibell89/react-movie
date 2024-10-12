acd6862d894c7a46f5290da19e013c41c5ae4490
jtroubleK <149900298+jbellido89@users.noreply.github.com>
2024-10-13 00:30:56 +0200

    Enhance UI layout and styles; add search functionality in App component to improve movie search experience and user interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import './App.css';
+import { useState } from 'react';
 import { useMovies } from './hooks/useMovies.js';
 import { Movies } from './components/Movies.jsx';
 
 function App() {
-  const { movies: mappedMovies } = useMovies();
+  const [query, setQuery] = useState('');
+  const { movies } = useMovies(query);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setQuery(event.target.elements.search.value);
+  };
+
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input type='text' placeholder='Avengers, Star Wars, Batman...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            name='search'
+            type='text'
+            placeholder='Avengers, Star Wars, Batman...'
+          />
           <button type='submit'>Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,8 +1,21 @@
-import responseMovies from '../mocks/with-results.json';
-import noResults from '../mocks/no-results.json';
+import { useState, useEffect } from 'react';
 
-export function useMovies() {
-  const movies = responseMovies.Search;
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const searchMovies = async () => {
+      if (query) {
+        const response = await fetch(
+          `http://www.omdbapi.com/?apikey=e1e1960b&s=${query}`
+        );
+        const data = await response.json();
+        setMovies(data.Search || []);
+      }
+    };
+
+    searchMovies();
+  }, [query]);
 
   //Mapeamos los datos de la API para no depender de su contrato dentro de nuestro componente
   const mappedMovies = movies?.map((movie) => ({
